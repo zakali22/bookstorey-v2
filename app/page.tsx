89d50e484bf8d1c7a13d2b4bc0fd3b565f6700c1
zakali22 <18152558+zakali22@users.noreply.github.com>
2026-01-03 17:45:00 +0000

    Add trending books to the Popular page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,14 @@
 import Book from "@/components/domain/book/Book";
 import CategoriesListing from "@/components/domain/category/CategoriesListing";
-import getAllBooksById from "@/lib/actions/getAllBooks";
-import { getClient } from "@/lib/queries/apollo-setup"
-import GET_TRENDING_BOOKS from "@/lib/queries/books/getTrendingBooks.graphql";
-import { Book as BookType, BooksData, BooksTrendingData } from "@/lib/types/books";
+import getAllTrendingBooks from "@/lib/actions/getAllTrendingBooks";
 
 export default async function Home() {
-  const { data } = await getClient().query<BooksTrendingData>({ query: GET_TRENDING_BOOKS });
-
-  if(!data || data === undefined) return <p>Loading...</p>
-  
-  const books = await getAllBooksById(data.books_trending.ids)
-
-  const dedupedListing = new Map<string, BookType>()
-  books.filter((book): book is BooksData => book !== undefined).forEach(({books_by_pk: book}) => {
-      if(book && !dedupedListing.has(book.id.toString())){
-          dedupedListing.set(book.id.toString(), book)
-      }
-  })
-
-  console.log("books listing =>", dedupedListing)
+  const trendingBooks = await getAllTrendingBooks()
 
   return (
     <>
     <div className="grid grid-cols-10 min-h-screen items-start justify-start space-x-4">
-      {Array.from(dedupedListing).map((book) => (
+      {trendingBooks && Array.from(trendingBooks).map((book) => (
           <Book {...book[1]} key={parseInt(book[0])} />
       ))}
     </div>
--- app/popular/page.tsx
+++ app/popular/page.tsx
@@ -1,3 +1,16 @@
-export default function Popular(){
-    return <h1>Popular page</h1>
+import Book from "@/components/domain/book/Book";
+import getAllTrendingBooks from "@/lib/actions/getAllTrendingBooks";
+
+export default async function Popular() {
+  const trendingBooks = await getAllTrendingBooks({ limit: 100 })
+
+  return (
+    <>
+    <div className="grid grid-cols-10 min-h-screen items-start justify-start space-x-4">
+      {trendingBooks && Array.from(trendingBooks).map((book) => (
+          <Book {...book[1]} key={parseInt(book[0])} />
+      ))}
+    </div>
+    </>
+  );
 }
